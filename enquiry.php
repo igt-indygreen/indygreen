7986e3bec445effba8f6a2121c7fa8951222e3b0
igt-indygreen <digitaligt35@gmail.com>
2025-10-10 14:51:45 +0530

    c

--- enquiry.php
+++ enquiry.php
@@ -1,38 +1,47 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $to = "digitaligt35@gmail.com"; // 🔹 Change this to your email address
-    $subject = "New Enquiry from Website";
 
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $phone = htmlspecialchars($_POST['phone']);
-    $message = htmlspecialchars($_POST['message']);
-    $enquiry_for = htmlspecialchars($_POST['enquiry_for']);
+if(!empty($_POST['xxx']))
+{
+   $compny = $_POST['company'];
+      $name = $_POST['name'];
+         $eml = $_POST['email'];
+            $mbl = $_POST['mobile'];  
+              $ps = $_POST['ps'];
+               $msg = $_POST['message'];
+               $tomail = "digitaligt35@gmail.com";
+               
+             
+                 
+               $kailash = "Company Name:".$compny.
+               "\r\n Full Name: ".$name.
+               "\r\n Email : ".$eml.
+               "\r\n Mobile :".$mbl.
+               "\r\n Product & Service Name :".$ps.
+               "\r\n Message :".$msg. "\r\n" ;
+               
+              $headers = [
+     
+                  "MIME-Version : 1.0",
+                  "Content-type : text/plain; charset=utf-8",
+                  "Cc : digitaligt35@gmail.com",
+               ];
 
-    $body = "
-    You have received a new enquiry:
-    ----------------------------------
-    Enquiry For: $enquiry_for
-    Name: $name
-    Email: $email
-    Phone: $phone
-    Message: 
-    $message
-    ----------------------------------
-    Sent from your website form.
-    ";
-
-    $headers = "From: $name <$email>\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
-
-    if (mail($to, $subject, $body, $headers)) {
-        header("Location: enquiry.html?success=1");
-        exit();
-    } else {
-        echo "Error: Unable to send email. Please try again later.";
-    }
-} else {
-    echo "Invalid Request.";
+         $headers = implode("\r\n", $headers);
+               
+               if(mail($tomail, $name, $kailash, $headers))
+               {
+                   echo "mail have sended...!";
+               }
+               else
+               {
+                   
+                   echo "mail failed ..!";
+               }
+               
+               
+               
+               
 }
+
+
 ?>
